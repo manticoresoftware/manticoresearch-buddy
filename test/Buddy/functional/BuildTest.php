f18a9c2b2184768e839e8d818cc1b17534e67981
Don Hardman <don.hardman@muvon.io>
2023-01-02 07:26:36 +0000

    Test build results and composer packages in it

--- test/Buddy/functional/BuildTest.php
+++ test/Buddy/functional/BuildTest.php
@@ -16,5 +16,45 @@ use PHPUnit\Framework\TestCase;
  * And this version of binary run and also does not include any of dev composer deps in it
  */
 final class BuildTest extends TestCase {
+	public function testBuildIsOk(): void {
+		static::buildBinary();
+		$this->assertEquals(true, file_exists('build/manticore-buddy.phar'));
+		$this->assertEquals(true, file_exists('build/manticore-buddy'));
+	}
 
+	public function testBuildHasRightComposerPackages(): void {
+		/** @var array{require:array<string,string>,require-dev:array<string,string>} $composer */
+		$composer = json_decode((string)file_get_contents('/workdir/composer.json'), true);
+		$include = array_keys($composer['require']);
+		$exclude = array_keys($composer['require-dev']);
+		/** @var array{dev:bool,dev-package-names:array<string,string>} $installed */
+		$installed = json_decode(
+			(string)file_get_contents('phar:///workdir/build/manticore-buddy.phar/vendor/composer/installed.json'),
+			true
+		);
+		$this->assertEquals(false, $installed['dev']);
+		$this->assertEquals([], $installed['dev-package-names']);
+
+		$phar = new Phar('build/manticore-buddy.phar', 0);
+		$packages = [];
+		/** @var SplFileInfo $file */
+		foreach (new RecursiveIteratorIterator($phar) as $file) {
+			$pos = strpos((string)$file, 'manticore-buddy.phar/vendor/');
+			if ($pos === false) {
+				continue;
+			}
+
+			$ns = strtok(substr((string)$file, $pos + 28), '/');
+			$name = strtok('/');
+			$packages["$ns/$name"] = true;
+		}
+
+		$packages = array_keys($packages);
+		$this->assertEquals([], array_diff($include, $packages));
+		$this->assertEquals($exclude, array_diff($exclude, $packages));
+	}
+
+	protected static function buildBinary(): void {
+		system('bin/build --name="Manticore Buddy" --package="manticore-buddy" --index="src/main.php"');
+	}
 }
