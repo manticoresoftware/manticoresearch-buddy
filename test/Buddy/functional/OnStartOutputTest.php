a5e357ed323b746f9bd1bc2d488ea07bb24d6f01
Nick-S-2018 <45552280+Nick-S-2018@users.noreply.github.com>
2023-01-05 14:57:04 +0700

    Added tests for Network/Server (#68)
    * Added tests for Network/Server
    
    * Updated Network/Server tests, added a functional test
    
    Co-authored-by: Nick Sergeev <nick@manticoresearch.com>

---
+++ test/Buddy/functional/OnStartOutputTest.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types=1);
+
+/*
+ Copyright (c) 2022, Manticore Software LTD (https://manticoresearch.com)
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 2 or any later
+ version. You should have received a copy of the GPL license along with this
+ program; if you did not, you can find it at http://www.gnu.org/
+ */
+
+use Manticoresearch\BuddyTest\Trait\TestFunctionalTrait;
+use PHPUnit\Framework\TestCase;
+
+class OnStartOutputTest extends TestCase {
+
+	use TestFunctionalTrait;
+
+	/**
+	 * @var string $searchdLog
+	 */
+	protected string $searchdLog;
+
+	/**
+	 * @var string $searchdLogFilepath
+	 */
+	protected string $searchdLogFilepath;
+
+	public function setUp(): void {
+		$conf = (string)file_get_contents(self::$manticoreConfigFile);
+		preg_match('/log = (.*?)[\r\n]/', $conf, $matches);
+		$this->searchdLogFilepath = $matches[1];
+		$this->searchdLog = (string)file_get_contents($this->searchdLogFilepath);
+	}
+
+	public function testConnectionAddressOutput(): void {
+		echo "\nTesting if connection address info is passed from Buddy to daemon correctly\n";
+		// Restart buddy with a new connection address
+		self::tearDownAfterClass();
+		self::setUpBeforeClass();
+		// Checking the log part corresponding to the latest searchd start
+		$logUpdate = $this->getLogUpdate();
+		$matches = [];
+		preg_match('/\[BUDDY\] started (.*?) at (.*?)$/', $logUpdate, $matches);
+		$this->assertCount(3, $matches);
+		$addr1 = $matches[2];
+
+		// Checking if Buddy passes a refreshed connection address on restart
+		self::tearDownAfterClass();
+		self::setUpBeforeClass();
+		$logUpdate = $this->getLogUpdate();
+		preg_match('/\[BUDDY\] started (.*) at (.*?)$/', $logUpdate, $matches);
+		$this->assertCount(3, $matches);
+		$addr2 = $matches[2];
+
+		$this->assertNotEquals($addr1, $addr2);
+	}
+
+	public function testTickersOutput(): void {
+		echo "\nTesting if the output from ticker functions is passed from Buddy to daemon correctly\n";
+		// Setting debug mode in Manticore config
+		$conf = (string)file_get_contents(self::$manticoreConfigFile);
+		$conf = preg_replace('/buddy_path(.*?)(\r|\n)/', 'buddy_path$1 --debug$2', $conf);
+		file_put_contents(self::$manticoreConfigFile, $conf);
+		self::tearDownAfterClass();
+		self::setUpBeforeClass();
+		// Unsetting debug mode in manticore config
+		$conf = (string)file_get_contents(self::$manticoreConfigFile);
+		$conf = str_replace(' --debug', '', $conf);
+		file_put_contents(self::$manticoreConfigFile, $conf);
+
+		ob_flush();
+		sleep(70);
+		$logUpdate = $this->getLogUpdate();
+		$this->assertStringContainsString('[BUDDY] memory usage:', $logUpdate);
+		$this->assertEquals(1, preg_match('/running \d+ tasks$/', $logUpdate));
+	}
+
+	/**
+	 * Helper function to retrieve the updated part of the searchd log
+	 *
+	 * @return string
+	 */
+	protected function getLogUpdate(): string {
+		$curLog = (string)file_get_contents($this->searchdLogFilepath);
+		$logUpdate = str_replace($this->searchdLog, '', $curLog);
+		$this->searchdLog = $curLog;
+		return $logUpdate;
+	}
+}
