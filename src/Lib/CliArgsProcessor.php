19ac10c0ac99dbf3b472fc15969c4e8fd7b0b9ad
Don Hardman <don.hardman@muvon.io>
2023-01-06 13:30:21 +0000

    Add tests and also rethink how we test CliArgsProcessor

--- src/Lib/CliArgsProcessor.php
+++ src/Lib/CliArgsProcessor.php
@@ -59,28 +59,41 @@ final class CliArgsProcessor {
 	/**
 	 * Process cli arguments passed
 	 *
+	 * @param ?array{
+	 *  threads?:int,
+	 *  telemetry-period?:int,
+	 *  disable-telemetry?:bool,
+	 *  debug?:bool,
+	 *  help?:bool,
+	 *  version?:bool,
+	 *  listen?:string
+	 * } $opts
 	 * @return array{
-	 * threads?:int,
-	 * telemetry-period?:int,
-	 * disable-telemetry?:bool,
-	 * debug?:bool,
-	 * help?:bool,
-	 * version?:bool,
-	 * listen:string
+	 *  threads?:int,
+	 *  telemetry-period?:int,
+	 *  disable-telemetry?:bool,
+	 *  debug?:bool,
+	 *  help?:bool,
+	 *  version?:bool,
+	 *  listen:string
 	 * }
 	 */
-	public static function run(): array {
+	public static function run(?array $opts = null): array {
+		if (!isset($opts)) {
+			$opts = getopt('', self::LONG_OPTS);
+		}
 		/** @var array{
-		 * threads?:int,
-		 * telemetry-period?:int,
-		 * disable-telemetry?:bool,
-		 * debug?:bool,
-		 * help?:bool,
-		 * version?:bool,
-		 * listen:string
-		 * }
+		 *  threads?:int,
+		 *  telemetry-period?:int,
+		 *  disable-telemetry?:bool,
+		 *  debug?:bool,
+		 *  help?:bool,
+		 *  version?:bool,
+		 *  listen:string
+		 * } $opts
 		 */
-		$opts = array_replace(self::DEFAULT_OPTS, getopt('', self::LONG_OPTS));
+		$opts = array_replace(self::DEFAULT_OPTS, $opts); // @phpstan-ignore-line
+
 		if (isset($opts['help'])) {
 			echo self::help();
 			exit(0);
@@ -91,37 +104,80 @@ final class CliArgsProcessor {
 			exit(0);
 		}
 
-		if (isset($opts['threads'])) {
-			if ($opts['threads'] < 2 || $opts['threads'] > 256) {
-				echo "The --threads must be in range of 2 to 256 secs.\n";
-				exit(1);
-			}
 
-			putenv('THREADS=' . (int)$opts['threads']);
+		static::parseThreads($opts);
+		static::parseDisableTelemetry($opts);
+		static::parseDebug($opts);
+		static::parseTelemetryPeriod($opts);
+		static::parseListen($opts);
+
+		return $opts;
+	}
+
+	/**
+	 * @param array{threads?:int} $opts
+	 * @return void
+	 */
+	protected static function parseThreads(array $opts): void {
+		if (!isset($opts['threads'])) {
+			return;
 		}
 
+		if ($opts['threads'] < 2 || $opts['threads'] > 256) {
+			echo "The --threads must be in range of 2 to 256 secs.\n";
+			exit(1);
+		}
+
+		putenv('THREADS=' . (int)$opts['threads']);
+	}
+
+	/**
+	 * @param array{disable-telemetry?:bool} $opts
+	 * @return void
+	 */
+	protected static function parseDisableTelemetry(array $opts): void {
 		if (isset($opts['disable-telemetry'])) {
 			putenv('TELEMETRY=0');
 		} else {
 			putenv('TELEMETRY=1');
 		}
+	}
 
-		if (isset($opts['debug'])) {
-			putenv('DEBUG=1');
+	/**
+	 * @param array{debug?:bool} $opts
+	 * @return void
+	 */
+	protected static function parseDebug(array $opts): void {
+		if (!isset($opts['debug'])) {
+			return;
+		}
+		putenv('DEBUG=1');
+	}
+
+	/**
+	 * @param array{telemetry-period?:int} $opts
+	 * @return void
+	 */
+	protected static function parseTelemetryPeriod(array $opts): void {
+		if (!isset($opts['telemetry-period'])) {
+			return;
 		}
 
-		if (isset($opts['telemetry-period'])) {
-			if ($opts['telemetry-period'] < 5 || $opts['telemetry-period'] > 1800) {
-				echo "The --telemetry-period must be in range of 5 to 1800 secs.\n";
-				exit(1);
-			}
-			putenv('TELEMETRY_PERIOD=' . (int)$opts['telemetry-period']);
+		if ($opts['telemetry-period'] < 5 || $opts['telemetry-period'] > 1800) {
+			echo "The --telemetry-period must be in range of 5 to 1800 secs.\n";
+			exit(1);
 		}
+		putenv('TELEMETRY_PERIOD=' . (int)$opts['telemetry-period']);
+	}
 
+	/**
+	 * @param array{listen:string} $opts
+	 * @return void
+	 */
+	protected static function parseListen(array $opts): void {
 		if (str_starts_with($opts['listen'], 'http://0.0.0.0')) {
 			$opts['listen'] = 'http://127.0.0.1' . substr($opts['listen'], 14);
 		}
 		putenv("LISTEN={$opts['listen']}");
-		return $opts;
 	}
 }
