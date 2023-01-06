e773d54d4cdd72f4c893c5941cf09c6cc33b0f37
Don Hardman <don.hardman@muvon.io>
2023-01-06 13:30:21 +0000

    Add tests and also rethink how we test CliArgsProcessor

--- test/Buddy/src/Lib/CliArgsProcessorTest.php
+++ test/Buddy/src/Lib/CliArgsProcessorTest.php
@@ -17,37 +17,68 @@ class CliArgsProcessorTest extends TestCase {
 
 	use TestProtectedTrait;
 
-	protected function setUp():void {
-		CliArgsProcessor::run();
-	}
-
 	protected function tearDown():void {
 		putenv('TELEMETRY');
+		putenv('TELEMETRY_PERIOD');
 		putenv('DEBUG');
 		putenv('LISTEN');
+		putenv('THREADS');
 	}
 
 	public function testDefaultArgsProcessOk(): void {
+		CliArgsProcessor::run();
+
 		echo "\nTesting the processing of the arguments with default values\n";
 		$this->assertEquals(1, getenv('TELEMETRY'));
 		$this->assertEquals(false, getenv('DEBUG'));
 	}
 
 	public function testListenArgProcessOk(): void {
+		CliArgsProcessor::run();
+
 		echo "\nTesting the processing of the `listen` argument\n";
 		$refCls = new \ReflectionClass(CliArgsProcessor::class);
 		$consts = $refCls->getConstant('DEFAULT_OPTS');
 		if (is_array($consts)) {
-			$this->assertEquals($consts['listen'], getenv('LISTEN'));
+			$this->assertEquals($consts['listen'], getenv('LISTEN', true));
 		}
-		$this->assertEquals('127.0.0.1:9308', getenv('LISTEN'));
+		$this->assertEquals('127.0.0.1:9308', getenv('LISTEN', true));
+
+		$listen = '10.0.0.1:34445';
+		CliArgsProcessor::run(['listen' => $listen]);
+		$this->assertEquals($listen, getenv('LISTEN', true));
 	}
 
 	public function testVersionArgProcessOk(): void {
+		CliArgsProcessor::run();
+
 		echo "\nTesting the processing of the `version` argument\n";
 		$version = trim((string)file_get_contents(__DIR__ . '/../../../../APP_VERSION'));
 		$res = "Manticore Buddy v$version\n"
 			. "Copyright (c) 2022, Manticore Software LTD (https://manticoresearch.com)\n";
 		$this->assertEquals($res, self::invokeMethod(CliArgsProcessor::class, 'version'));
+	}
+
+	public function testThreadsArgProcessOk(): void {
+		$threads = mt_rand(5, 10);
+		CliArgsProcessor::run(['threads' => $threads]);
+
+		echo "\nTesting the processing of the `threads` argument\n";
+		$this->assertEquals($threads, (int)getenv('THREADS', true));
+	}
+
+	public function testDisableTelemetryArgProcessOk(): void {
+		CliArgsProcessor::run(['disable-telemetry' => false]);
+
+		echo "\nTesting the processing of the `disable-telemetry` argument\n";
+		$this->assertEquals('0', getenv('TELEMETRY', true));
+	}
+
+	public function testTelemetryPeriodArgProcessOk(): void {
+		$period = mt_rand(5, 180);
+		CliArgsProcessor::run(['telemetry-period' => $period]);
+
+		echo "\nTesting the processing of the `telemetry-period` argument\n";
+		$this->assertEquals($period, (int)getenv('TELEMETRY_PERIOD', true));
 	}
 }
