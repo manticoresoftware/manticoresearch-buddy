3a70098966ecc787aa9603c11521b3a764296753
Nick-S-2018 <45552280+Nick-S-2018@users.noreply.github.com>
2023-01-09 19:31:35 +0700

    Updated tests using manticore-debug config (#75)
    Co-authored-by: Nick Sergeev <nick@manticoresearch.com>

--- test/Buddy/functional/DebugModeTest.php
+++ test/Buddy/functional/DebugModeTest.php
@@ -27,59 +27,44 @@ class DebugModeTest extends TestCase {
 	protected string $searchdLogFilepath;
 
 	/**
-	 * @var int $debugMode
+	 * @var ?string $configFileName
 	 */
-	protected static int $debugMode = 0;
+	protected static ?string $configFileName = null;
 
 	public function setUp(): void {
 		$conf = (string)file_get_contents(self::$manticoreConfigFile);
 		preg_match('/log = (.*?)[\r\n]/', $conf, $matches);
 		$this->searchdLogFilepath = $matches[1];
 		$this->searchdLog = (string)file_get_contents($this->searchdLogFilepath);
-		if (self::$debugMode === 1) {
-			// Setting debug mode in manticore config
-			$conf = (string)file_get_contents(self::$manticoreConfigFile);
-			$conf = preg_replace('/buddy_path(.*?)(\r|\n)/', 'buddy_path$1 --debug$2', $conf);
-			file_put_contents(self::$manticoreConfigFile, $conf);
-			self::$debugMode = 2;
-		}
 		self::setUpBeforeClass();
 	}
 
 	public function tearDown(): void {
 		self::tearDownAfterClass();
-		if (self::$debugMode !== 2) {
-			return;
-		}
-		// Unsetting debug mode in manticore config
-		$conf = (string)file_get_contents(self::$manticoreConfigFile);
-		$conf = str_replace(' --debug', '', $conf);
-		file_put_contents(self::$manticoreConfigFile, $conf);
-		self::$debugMode = 0;
 	}
 
-// 	public function testDebugModeOff(): void {
-// 		echo "\nTesting the Buddy log output without debug mode enabled\n";
-// 		ob_flush();
-// 		// Waiting for the possible debug message to come
-// 		sleep(70);
-// 		// Checking the log part corresponding to the latest searchd start
-// 		$logUpdate = str_replace($this->searchdLog, '', (string)file_get_contents($this->searchdLogFilepath));
-// 		$this->assertStringNotContainsString('[BUDDY] memory usage:', $logUpdate);
-// 		self::$debugMode = 1;
-// 	}
+	public function testDebugModeOff(): void {
+		echo "\nTesting the Buddy log output without debug mode enabled\n";
+		ob_flush();
+		// Waiting for the possible debug message to come
+		sleep(70);
+		// Checking the log part corresponding to the latest searchd start
+		$logUpdate = str_replace($this->searchdLog, '', (string)file_get_contents($this->searchdLogFilepath));
+		$this->assertStringNotContainsString('[BUDDY] memory usage:', $logUpdate);
+		self::$configFileName = 'manticore-debug.conf';
+	}
 
-// 	/**
-// 	 * @depends testDebugModeOff
-// 	 */
-// 	public function testDebugModeOn(): void {
-// 		echo "\nTesting the Buddy log output with debug mode enabled\n";
-// 		ob_flush();
-// 		// Waiting for the possible debug message to come
-// 		sleep(70);
-// 		// Checking the log part corresponding to the latest searchd start
-// 		$logUpdate = str_replace($this->searchdLog, '', (string)file_get_contents($this->searchdLogFilepath));
-// 		$this->assertStringContainsString('[BUDDY] memory usage:', $logUpdate);
-// 	}
+	/**
+	 * @depends testDebugModeOff
+	 */
+	public function testDebugModeOn(): void {
+		echo "\nTesting the Buddy log output with debug mode enabled\n";
+		ob_flush();
+		// Waiting for the possible debug message to come
+		sleep(70);
+		// Checking the log part corresponding to the latest searchd start
+		$logUpdate = str_replace($this->searchdLog, '', (string)file_get_contents($this->searchdLogFilepath));
+		$this->assertStringContainsString('[BUDDY] memory usage:', $logUpdate);
+	}
 
 }
