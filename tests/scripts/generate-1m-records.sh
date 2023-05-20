704e84a705298536c26c1e69ccfcc31b1ff88639
Don Hardman <don.hardman@muvon.io>
2023-05-20 10:21:23 +0000

    Use prepared dump for tests to fix performance issue

--- tests/scripts/generate-1m-records.sh
+++ tests/scripts/generate-1m-records.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-lcg_seed=1024
-lcg_m=$((2**32))
-lcg_a=1103515245
-state_file=$(mktemp)
-
-# Initialize state file with 0
-echo 0 > "$state_file"
-
-# Function to generate pseudo-random numbers.
-lcg() {
-  local lcg_c
-  read -r lcg_c < "$state_file"
-  lcg_seed=$(((lcg_a*lcg_seed+lcg_c)%lcg_m))
-  echo $((lcg_seed % 32768))
-  echo $((lcg_c+1)) > "$state_file"
-}
-
+RANDOM=1024
 id=0
 
 for n in `seq 1 10`; do
@@ -33,11 +17,10 @@ process_batch() {
     for id in $(seq $start_id $end_id); do
       random_text=$(
         for i in {1..3}; do
-          num=$(( id % i ))
-          printf "\\$(printf '%03o' $((`lcg` % 26 + 65 + `lcg` % 2 * 32)))";
+          printf "\\$(printf '%03o' $((RANDOM % 26 + 65 + RANDOM % 2 * 32)))";
         done; echo
       )
-      echo -n "($id, '$s',`lcg`, 1.$((`lcg` % 1000)), '{\"a\": [`lcg`,`lcg`], \"b\": `lcg`}', (`lcg`,`lcg`,`lcg`), '$random_text')"
+      echo -n "($id, '$s',$RANDOM, 1.$((RANDOM % 1000)), '{\"a\": [$RANDOM,$RANDOM], \"b\": $RANDOM}', ($RANDOM,$RANDOM,$RANDOM), '$random_text')"
       [ $id != $end_id ] && echo -n ","
     done
     echo ";"
@@ -45,7 +28,7 @@ process_batch() {
   echo -n .
 }
 
-for m in $(seq 1 600); do
+for m in $(seq 1 700); do
   start_id=$((id + 1))
   id=$((id + 1500))
   process_batch $start_id $id
