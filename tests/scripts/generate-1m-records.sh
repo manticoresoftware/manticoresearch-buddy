10400b1fcbffad5c60811cb7955efa76f35b2cc2
Don Hardman <don.hardman@muvon.io>
2023-05-22 06:55:28 +0000

    Use manticore-executor for random generation and update tests

--- tests/scripts/generate-1m-records.sh
+++ tests/scripts/generate-1m-records.sh
@@ -1,36 +1,33 @@
-#!/usr/bin/env bash
+#!/usr/bin/env manticore-executor
+<?php
+srand(1024);
 
-RANDOM=1024
-id=0
+$id=0;
+echo "create table t(f text, a int, b float, j json, m multi, s string);\n";
+for ($i = 0; $i < 1000000; $i++) {
+  if ($i % 10000 === 0) {
+    if ($i !== 0) {
+      echo ";\n";
+    }
+    echo 'insert into t (id, f, a, b, j, m, s) values ';
+  } else {
+    echo ',';
+  }
 
-for n in `seq 1 10`; do
-  s="a$s";
-done;
-
-mysql -P9306 -h0 -e "create table t(f text, a int, b float, j json, m multi, s string)";
-
-process_batch() {
-  start_id=$1
-  end_id=$2
-  (
-    echo -n "insert into t (id, f, a, b, j, m, s) values "
-    for id in $(seq $start_id $end_id); do
-      random_text=$(
-        for i in {1..3}; do
-          printf "\\$(printf '%03o' $((RANDOM % 26 + 65 + RANDOM % 2 * 32)))";
-        done; echo
-      )
-      echo -n "($id, '$s',$RANDOM, 1.$((RANDOM % 1000)), '{\"a\": [$RANDOM,$RANDOM], \"b\": $RANDOM}', ($RANDOM,$RANDOM,$RANDOM), '$random_text')"
-      [ $id != $end_id ] && echo -n ","
-    done
-    echo ";"
-  ) | mysql -P9306 -h0
-  echo -n .
+  $rand1 = rand() / 10000;
+  $rand2 = rand();
+  $rand3 = rand();
+  $rand4 = rand();
+  $rand5 = rand();
+  $rand6 = rand();
+  $rand7 = rand();
+  $rand8 = rand();
+  $s = str_repeat('a', $rand4 % 10);
+  $random_text = '';
+  for ($j = 0; $j < 3; $j++) {
+    $n = $j + 2;
+    $var = "rand$n";
+    $random_text .= chr($$var % 26 + 65 + $$var % 2 * 32);
+  }
+  echo "(" . ++$id . ", '$s', $rand1, $rand2, '{\"a\": [$rand3,$rand4], \"b\": $rand5}', ($rand6,$rand7,$rand8), '$random_text')";
 }
-
-for m in $(seq 1 700); do
-  start_id=$((id + 1))
-  id=$((id + 1500))
-  process_batch $start_id $id
-done
-echo
