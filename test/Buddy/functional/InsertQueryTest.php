de0339a0a3d3faea00ef4fb6ca108181334a60cb
Don Hardman <don.hardman@muvon.io>
2024-02-07 16:38:45 +0700

    Fix tests for fix/error branch of buddy-core

--- test/Buddy/functional/InsertQueryTest.php
+++ test/Buddy/functional/InsertQueryTest.php
@@ -54,7 +54,7 @@ class InsertQueryTest extends TestCase {
 		echo "\nTesting the fail on the execution of HTTP insert query to a non-existing table\n";
 		$query = "INSERT into {$this->testTable}(col1) VALUES(1,2) ";
 		$out = static::runHttpQuery($query);
-		$result = [['total' => 0,'error' => "P01: wrong number of values here near ') '",'warning' => '']];
+		$result = ['error' => "P01: wrong number of values here near ') '"];
 		$this->assertEquals($result, $out);
 	}
 
