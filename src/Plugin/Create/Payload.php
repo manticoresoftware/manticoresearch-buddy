fa0c2b5f1f93480e5f5e999dd7987e388f225c6f
djklim87 <klim@manticoresearch.com>
2024-04-24 23:28:33 +0200

    Stable create like with data

---
+++ src/Plugin/Create/Payload.php
@@ -0,0 +1,168 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
+namespace Manticoresearch\Buddy\Base\Plugin\Create;
+
+use Manticoresearch\Buddy\Core\ManticoreSearch\RequestFormat;
+use Manticoresearch\Buddy\Core\Network\Request;
+use Manticoresearch\Buddy\Core\Plugin\BasePayload;
+
+/**
+ * This is simple do nothing request that handle empty queries
+ * which can be as a result of only comments in it that we strip
+ */
+final class Payload extends BasePayload
+{
+	public string $path;
+
+	public string $destinationTableName;
+	public string $sourceTableName;
+
+	public string $type;
+
+	/**
+	 * Get description for this plugin
+	 * @return string
+	 */
+	public static function getInfo(): string {
+		return 'Enables partial replaces';
+	}
+
+	/**
+	 * @param Request $request
+	 * @return static
+	 */
+	public static function fromRequest(Request $request): static {
+		$self = new static();
+		$self->path = $request->path;
+		$self->type = $request->format->value;
+
+		if ($request->format->value === RequestFormat::SQL->value) {
+			$payload = self::parsePayload($request->payload);
+
+			$self->destinationTableName = $payload['TABLE']['no_quotes']['parts'][0];
+			$self->sourceTableName = $payload['LIKE']['no_quotes']['parts'][0];
+		} else {
+			// TODO check http request
+		}
+		return $self;
+	}
+
+	/**
+	 * @param Request $request
+	 * @return bool
+	 */
+	public static function hasMatch(Request $request): bool {
+
+		if ($request->format->value === RequestFormat::SQL->value) {
+			$payload = self::parsePayload($request->payload);
+
+			if (isset($payload['CREATE'])
+				&& isset($payload['TABLE']['no_quotes']['parts'][0])
+				&& isset($payload['LIKE']['no_quotes']['parts'][0])
+				&& isset($payload['TABLE']['options'][0]['base_expr'])
+				&& $payload['TABLE']['options'][0]['base_expr'] === 'WITH DATA'
+			) {
+				return true;
+			}
+
+			return false;
+		}
+
+		return str_contains($request->path, '/_update/');
+	}
+
+	/**
+	 * @param string $stringPayload
+	 * @return array{
+	 *      CREATE?: array{
+	 *          expr_type: string,
+	 *          not-exists: bool,
+	 *          base_expr: string,
+	 *          sub_tree?: array<array{
+	 *              expr_type: string,
+	 *              base_expr: string
+	 *          }>
+	 *      },
+	 *      TABLE?: array{
+	 *          base_expr: string,
+	 *          name?: string,
+	 *          no_quotes: array{
+	 *              delim: bool,
+	 *              parts: array<string>
+	 *          },
+	 *          create-def?: bool,
+	 *          options?: array<array{
+	 *              expr_type: string,
+	 *              base_expr: string,
+	 *              delim: string,
+	 *              sub_tree?: array<array{
+	 *                  expr_type: string,
+	 *                  base_expr: string
+	 *              }>
+	 *          }>
+	 *      },
+	 *      LIKE?: array{
+	 *          expr_type: string,
+	 *          table?: string,
+	 *          base_expr: string,
+	 *          no_quotes: array{
+	 *              delim: bool,
+	 *              parts: array<string>
+	 *          }
+	 *      }
+	 *  }
+	 */
+	public static function parsePayload(string $stringPayload): array {
+		/**
+		 * @var array{
+		 *       CREATE: array{
+		 *           expr_type: string,
+		 *           not-exists: bool,
+		 *           base_expr: string,
+		 *           sub_tree?: array<array{
+		 *               expr_type: string,
+		 *               base_expr: string
+		 *           }>
+		 *       },
+		 *       TABLE: array{
+		 *           base_expr: string,
+		 *           name?: string,
+		 *           no_quotes: array{
+		 *               delim: bool,
+		 *               parts: array<string>
+		 *           },
+		 *           create-def?: bool,
+		 *           options?: array<array{
+		 *               expr_type: string,
+		 *               base_expr: string,
+		 *               delim: string,
+		 *               sub_tree?: array<array{
+		 *                   expr_type: string,
+		 *                   base_expr: string
+		 *               }>
+		 *           }>
+		 *       },
+		 *       LIKE: array{
+		 *           expr_type: string,
+		 *           table?: string,
+		 *           base_expr: string,
+		 *           no_quotes: array{
+		 *               delim: bool,
+		 *               parts: array<string>
+		 *           }
+		 *       }
+		 *   } $payload
+		 */
+		return static::$sqlQueryParser::parse($stringPayload);
+	}
+
+}
