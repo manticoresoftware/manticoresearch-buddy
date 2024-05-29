85f707eb5de7404c3023811e943a9ed0f5e577af
Don Hardman <don.hardman@muvon.io>
2024-05-29 19:36:23 +0700

    Introduce MVP for AUTOCOMPLETE function to make suggestions based on input query

---
+++ src/Plugin/Autocomplete/Payload.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2024, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
+namespace Manticoresearch\Buddy\Base\Plugin\Autocomplete;
+
+use Manticoresearch\Buddy\Core\Error\QueryParseError;
+use Manticoresearch\Buddy\Core\Network\Request;
+use Manticoresearch\Buddy\Core\Plugin\BasePayload;
+
+/**
+ * @phpstan-extends BasePayload<array>
+ */
+final class Payload extends BasePayload {
+	/** @var string */
+	public string $table;
+
+	/** @var string */
+	public string $query;
+
+	/** @var int */
+	public int $distance = 2;
+
+	public function __construct() {
+	}
+
+	/**
+	 * Get description for this plugin
+	 * @return string
+	 */
+	public static function getInfo(): string {
+		return 'Autocomplete plugin that offers suggestions based on the starting query';
+	}
+
+	/**
+	 * @param Request $request
+	 * @return static
+	 */
+	public static function fromRequest(Request $request): static {
+		preg_match('/autocomplete\s*\(([\'"])([^\1]+?)\1\s*,\s*([\'"])([^\3]+?)\3\)/ius', $request->payload, $matches);
+		if (!$matches) {
+			throw new QueryParseError('Failed to parse query');
+		}
+
+		$self = new static();
+		$self->query = $matches[2];
+		$self->table = $matches[4];
+		return $self;
+	}
+
+	/**
+	 * @param Request $request
+	 * @return bool
+	 */
+	public static function hasMatch(Request $request): bool {
+		return $request->error === 'no such built-in procedure AUTOCOMPLETE'
+			&& stripos($request->payload, 'CALL AUTOCOMPLETE(') === 0
+		;
+	}
+}
