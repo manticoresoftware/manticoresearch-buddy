a42ee3019dbd551901bf48a496eb90ac66ff9b7c
Don Hardman <59067742+donhardman@users.noreply.github.com>
2025-01-09 15:03:16 +0700

    Add distributed insert plugin logic (#402)
    * Add distributed insert plugin logic
    
    * Optimize performance what we can do for now
    
    * Update composer deps
    
    * Fix bugs in distributed insert logic: id generation and improve assign
    
    * Add performance measure for hasMatch on each plugin in debugv mode
    
    * Fix issue with insertion of only one doc
    
    * Use CALL uuid_short for ID generation
    
    * Add support of SQL inserts into distributed tables
    
    * Update buddy-core version
    
    * Improve insert check for SQL
    
    * Fix routing issue to distributed table for bulk of elasticsearch

---
+++ src/Plugin/DistributedInsert/Handler.php
@@ -0,0 +1,347 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2023-present, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+namespace Manticoresearch\Buddy\Base\Plugin\DistributedInsert;
+
+use Manticoresearch\Buddy\Core\Error\ManticoreSearchClientError;
+use Manticoresearch\Buddy\Core\Error\ManticoreSearchResponseError;
+use Manticoresearch\Buddy\Core\Network\Struct;
+use Manticoresearch\Buddy\Core\Plugin\BaseHandlerWithFlagCache;
+use Manticoresearch\Buddy\Core\Task\Task;
+use Manticoresearch\Buddy\Core\Task\TaskResult;
+use RuntimeException;
+
+/** @package Manticoresearch\Buddy\Base\Plugin\DistributedInsert */
+final class Handler extends BaseHandlerWithFlagCache {
+
+	/**
+	 * Initialize the executor
+	 *
+	 * @param Payload $payload
+	 * @return void
+	 */
+	public function __construct(public Payload $payload) {
+	}
+
+  /**
+	 * Process the request
+	 *
+	 * @return Task
+	 * @throws RuntimeException
+	 */
+	public function run(): Task {
+		$taskFn = function (): TaskResult {
+			$requests = [];
+			$positions = [];
+			$n = 0;
+			foreach ($this->payload->batch as $table => $batch) {
+				[$cluster, $table] = $this->payload::parseCluster($table);
+				$shards = $this->getShards($table);
+				$shardCount = sizeof($shards);
+				$idPool = $this->getNewDocIds(sizeof($batch));
+				$rows = [];
+				foreach ($batch as $struct) {
+					/** @var Struct<string,string|int>|Struct<"index",array{_id:string|int,_index:string}|string|int> $struct */
+					if ($this->shouldAssignId($struct)) {
+						$id = $this->assignId($struct, $idPool);
+						$idStr = (string)$id;
+						$positions[$idStr] = [
+							'n' => $n++,
+							'table' => $table,
+							'cluster' => $cluster,
+						];
+						$shard = hexdec(substr(md5($idStr), 0, 8)) % $shardCount;
+						$info = $shards[$shard];
+						$shardName = $info['name'];
+						$this->assignTable($struct, $cluster, $shardName);
+					}
+					$rows[] = $struct->toJson();
+				}
+
+				if (!isset($info)) {
+					throw new ManticoreSearchClientError('Failed to prepare docs for insertion');
+				}
+
+				$requests[] = $this->getRequest($info, $rows);
+			}
+			return $this->processRequests($requests, $positions);
+		};
+
+		return Task::create($taskFn)->run();
+	}
+
+	/**
+	 * @param array{name:string,url:string} $info
+	 * @param array<string> $rows
+	 * @return array{url:string,path:string,request:string}
+	 */
+	protected function getRequest(array $info, array $rows): array {
+		return [
+			'url' => $info['url'],
+			// We use for sql and default for others
+			'path' => $this->payload->type === 'sql' ? 'bulk' : $this->payload->path,
+			'request' => implode(PHP_EOL, $rows) . PHP_EOL,
+		];
+	}
+
+	/**
+	 * Generate new document ids for document by using manticore query
+	 * @return array<int>
+	 * @throws ManticoreSearchClientError
+	 * @throws ManticoreSearchResponseError
+	 */
+	protected function getNewDocIds(int $count = 1): array {
+		$ids = [];
+		/** @var array{0:array{data:array<array{"uuid_short()":int}>}} */
+		$result = $this->manticoreClient->sendRequest("CALL UUID_SHORT($count)")->getResult();
+		foreach ($result[0]['data'] as $row) {
+			$ids[] = $row['uuid_short()'];
+		}
+		return $ids;
+	}
+
+	/**
+	 * Get all shards for current distributed table from the schema
+	 * @param string $table
+	 * @return array<array{name:string,url:string}>
+	 * @throws RuntimeException
+	 */
+	protected function getShards(string $table): array {
+		static $map = [];
+		if (isset($map[$table])) {
+			return $map[$table];
+		}
+		[$locals, $agents] = $this->parseShards($table);
+
+		$shards = [];
+		// Add locals first
+		foreach ($locals as $t) {
+			$shards[] = [
+				'name' => $t,
+				'url' => '',
+			];
+		}
+		// Add agents after
+		foreach ($agents as $agent) {
+			$ex = explode('|', $agent);
+			$host = strtok($ex[0], ':');
+			$port = (int)strtok(':');
+			$t = strtok(':');
+			$shards[] = [
+				'name' => (string)$t,
+				'url' => "$host:$port",
+			];
+		}
+		$map[$table] = $shards;
+		return $shards;
+	}
+
+	/**
+	 * Helper to parse shards and return local and remote agents for current table
+	 * @param string $table
+	 * @return array{0:array<string>,1:array<string>}
+	 */
+	protected function parseShards($table): array {
+		/** @var array{0:array{data:array<array{"Create Table":string}>}} */
+		$res = $this->manticoreClient->sendRequest("SHOW CREATE TABLE $table")->getResult();
+		$tableSchema = $res[0]['data'][0]['Create Table'] ?? '';
+		if (!$tableSchema) {
+			throw new RuntimeException("There is no such table: {$table}");
+		}
+		if (!str_contains($tableSchema, "type='distributed'")) {
+			throw new RuntimeException('The table is not distributed');
+		}
+
+		if (!preg_match_all("/local='(?P<local>[^']+)'|agent='(?P<agent>[^']+)'/ius", $tableSchema, $m)) {
+			throw new RuntimeException('Failed to match tables from the schema');
+		}
+		return [
+			array_filter($m['local']),
+			array_filter($m['agent']),
+		];
+	}
+
+	/**
+	 * @param array<array{url:string,path:string,request:string}> $requests
+	 * @param array<string|int,array{n:int,table:string,cluster:string}> $positions
+	 * @return TaskResult
+	 * @throws ManticoreSearchClientError
+	 */
+	protected function processRequests(array $requests, array $positions): TaskResult {
+		$result = match ($this->payload->type) {
+			'bulk' => $this->processBulk($requests, $positions),
+			'sql' => $this->processSql($requests, $positions),
+			default => $this->processSingle($requests, $positions),
+		};
+
+		return TaskResult::raw($result);
+	}
+
+	/**
+	 * @param array<array{url:string,path:string,request:string}> $requests
+	 * @param array<string|int,array{n:int,table:string,cluster:string}> $positions
+	 * @return array{took:int,errors:bool,items:array<array{index:array{_id:string,_index:string}}>}
+	 * @throws ManticoreSearchClientError
+	 */
+	protected function processBulk(array $requests, array $positions): array {
+		$start = hrtime(true);
+		$responses = $this->manticoreClient->sendMultiRequest($requests);
+		$items = [];
+		$errors = false;
+		foreach ($responses as $n => $response) {
+			/** @var array{errors:bool,items:array<array{index:array{_id:string,_index:string}}>} */
+			$current = $response->getResult()->toArray();
+			$errors = $errors || $current['errors'];
+			foreach ($current['items'] as &$item) {
+				if ($this->payload->type === 'bulk') {
+					$id = $item['index']['_id'];
+					$info = $positions[$id];
+					$index = $item['index'];
+					$index['_index'] = $info['cluster'] ? "{$info['cluster']}:{$info['table']}" : $info['table'];
+					$item['index'] = $index;
+					$n = $info['n'];
+					$items[$n] = $item;
+				} else {
+					/* $id = $item['insert']['id']; */
+					/* $info = $positions[$id]; */
+					/* $doc = $item['insert']; */
+					/* $doc['table'] = $info['cluster'] ? "{$info['cluster']}:{$info['table']}" : $info['table']; */
+					/* $item['insert'] = $doc; */
+					/* $n = $info['n']; */
+					/* $items[$n] = $item; */
+				}
+			}
+		}
+		$took = hrtime(true) - $start;
+		ksort($items);
+		return [
+			'took' => (int)($took / 1e6),
+			'errors' => $errors,
+			'items' => array_values($items),
+		];
+	}
+
+	/**
+	 * Process simple single request like insert or whatever
+	 * @param array<array{url:string,path:string,request:string}> $requests
+	 * @param array<string|int,array{n:int,table:string,cluster:string}> $positions
+	 * @return array<mixed>
+	 * @throws ManticoreSearchClientError
+	 */
+	protected function processSingle(array $requests, array $positions): array {
+		$responses = $this->manticoreClient->sendMultiRequest($requests);
+		/** @var array{_id:string,_index:string}|array{error:array{index:string},index:string} */
+		$result = $responses[0]->getResult()->toArray();
+		/** @var array{n:int,table:string,cluster:string} $info */
+		$info = array_pop($positions);
+		$table = $info['cluster'] ? "{$info['cluster']}:{$info['table']}" : $info['table'];
+		if (isset($result['error'])) {
+			$result['error']['index'] = $table;
+		} elseif ($this->payload->type === 'bulk') {
+			$result['_index'] = $table;
+		} else {
+			$result['table'] = $table;
+		}
+		return $result;
+	}
+
+	/**
+	 * @param array<array{url:string,path:string,request:string}> $requests
+	 * @param array<string|int,array{n:int,table:string,cluster:string}> $positions
+	 * @return array<mixed>
+	 * @throws ManticoreSearchClientError
+	 */
+	protected function processSql(array $requests, array $positions): array {
+		$result = $this->processBulk($requests, $positions);
+		if ($result['errors'] && $this->payload->type === 'sql') {
+			throw ManticoreSearchClientError::create('Failed to insert docs');
+		}
+
+		/** @var array<mixed> */
+		return TaskResult::none()->getStruct();
+	}
+
+	/**
+	 * @param Struct<string,string|int>|Struct<"index",array{_id:string|int,_index:string}|string|int> $struct
+	 * @param array<int> &$idPool
+	 * @return int
+	 */
+	protected function assignId(Struct $struct, array &$idPool): int {
+		$id = match (true) {
+			// _bulk
+			isset($struct['index']['_id']) => $struct['index']['_id'],
+			// insert, deletc etc
+			isset($struct['id']) => $struct['id'],
+			// bulk
+			isset($struct['insert']['id']) => $struct['insert']['id'],
+			default => array_pop($idPool),
+		};
+		// When id = 0 we generate
+		if (!$id) {
+			$id = array_pop($idPool);
+		}
+		if (!$id) {
+			throw new ManticoreSearchClientError('Failed to assign id');
+		}
+		if ($this->payload->type === 'bulk') {
+			/** @var Struct<"index",array{_id:string|int,_index:string}> $struct */
+			/** @var array{_id:string|int,_index:string} $index */
+			$index = $struct['index'];
+			$index['_id'] = "$id";
+			$struct['index'] = $index;
+		} elseif ($this->payload->type === 'sql') {
+			/** @var Struct<"insert",array{id:string|int}> $struct */
+			$insert = $struct['insert'];
+			$insert['id'] = (int)$id;
+			$struct['insert'] = $insert;
+		} else {
+			/** @var Struct<"id",string|int> $struct */
+			$struct['id'] = $id;
+		}
+		return (int)$id;
+	}
+
+	/**
+	 * @param Struct<string,string|int>|Struct<"index",array{_id:string|int,_index:string}|string|int> $struct
+	 * @param string $cluster
+	 * @param string $shardName
+	 * @return void
+	 */
+	protected function assignTable(Struct $struct, string $cluster, string $shardName): void {
+		$table = $cluster ? "$cluster:$shardName" : $shardName;
+		if ($this->payload->type === 'bulk') {
+			/** @var Struct<"index",array{_id:string|int,_index:string}> $struct */
+			/** @var array{_id:string|int,_index:string} $index */
+			$index = $struct['index'];
+			$index['_index'] = $table;
+			$struct['index'] = $index;
+		} elseif ($this->payload->type === 'sql') {
+			/** @var Struct<"insert",array{table:string}> $struct */
+			$insert = $struct['insert'];
+			/** @var array{table:string} $insert */
+			$insert['table'] = $table;
+			$struct['insert'] = $insert;
+		} else {
+			/** @var Struct<string,string|int> $struct */
+			$struct['table'] = $table;
+		}
+	}
+
+	/**
+	 * @param Struct<string,string|int>|Struct<"index",array{_id:string|int,_index:string}|string|int> $struct
+	 * @return bool
+	 */
+	protected function shouldAssignId(Struct $struct): bool {
+		if ($this->payload->type === 'bulk' && !isset($struct['index'])) {
+			return false;
+		}
+		return true;
+	}
+}
---
+++ src/Plugin/DistributedInsert/Payload.php
@@ -0,0 +1,255 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2024, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
+namespace Manticoresearch\Buddy\Base\Plugin\DistributedInsert;
+
+use Manticoresearch\Buddy\Core\Error\QueryParseError;
+use Manticoresearch\Buddy\Core\ManticoreSearch\Endpoint;
+use Manticoresearch\Buddy\Core\Network\Request;
+use Manticoresearch\Buddy\Core\Network\Struct;
+use Manticoresearch\Buddy\Core\Plugin\BasePayload;
+
+/**
+ * Request for Backup command that has parsed parameters from SQL
+ * @phpstan-type Batch array<non-falsy-string,non-empty-array<int<0,max>, Struct<int|string,mixed>>>
+ * @phpstan-extends BasePayload<array>
+ */
+final class Payload extends BasePayload {
+	/** @var string */
+	public string $path;
+
+	/** @var string */
+	public string $type;
+
+	/** @var Batch */
+	public array $batch;
+
+	public function __construct() {
+	}
+
+	/**
+	 * Get description for this plugin
+	 * @return string
+	 */
+	public static function getInfo(): string {
+		return 'The plugin enables data insertion into distributed sharded tables.';
+	}
+
+	/**
+	 * @param Request $request
+	 * @return static
+	 */
+	public static function fromRequest(Request $request): static {
+		$self = new static();
+		$self->path = $request->path;
+
+		$self->batch = static::parsePayload($request);
+		$self->type = match ($request->endpointBundle) {
+			Endpoint::Sql, Endpoint::Cli => 'sql',
+			Endpoint::Insert => 'insert',
+			Endpoint::Replace => 'replace',
+			Endpoint::Bulk => 'bulk',
+			Endpoint::Update => 'update',
+			Endpoint::Delete => 'delete',
+			default => throw new \Exception('Unsupported endpoint bundle'),
+		};
+
+		return $self;
+	}
+
+	/**
+	 * @param Request $request
+	 * @return bool
+	 */
+	public static function hasMatch(Request $request): bool {
+		$hasErrorMessage = str_contains($request->error, 'not support insert');
+		// Insert or Replace
+		$hasMatch = ($request->endpointBundle === Endpoint::Insert
+			|| $request->endpointBundle === Endpoint::Replace)
+			&& $hasErrorMessage
+		;
+
+		// Bulk
+		if (!$hasMatch) {
+			$hasMatch = $request->endpointBundle === Endpoint::Bulk
+				&& $hasErrorMessage;
+		}
+
+		// Update
+		if (!$hasMatch) {
+			$hasMatch = $request->endpointBundle === Endpoint::Update;
+		}
+
+		// Delete
+		if (!$hasMatch) {
+			$hasMatch = $request->endpointBundle === Endpoint::Delete;
+		}
+
+		// SQL
+		if (!$hasMatch) {
+			$isSqlEndpoint = $request->endpointBundle === Endpoint::Sql
+				|| $request->endpointBundle === Endpoint::Cli;
+			$hasMatch = $isSqlEndpoint
+				&& strpos($request->error, 'not support insert') !== true
+				&& strpos($request->payload, 'insert') === 0;
+		}
+
+		return $hasMatch;
+	}
+
+	/**
+	 * @param Request $request
+	 * @return Batch
+	 * @throws QueryParseError
+	 */
+	protected static function parsePayload(Request $request): array {
+		if ($request->endpointBundle === Endpoint::Bulk) {
+			return static::parseBulkPayload($request);
+		}
+
+		if ($request->endpointBundle === Endpoint::Cli
+			|| $request->endpointBundle === Endpoint::Sql) {
+			return static::parseSqlPayload($request);
+		}
+
+		return static::parseSinglePayload($request);
+	}
+
+	/**
+	 * @param Request $request
+	 * @return Batch
+	 */
+	protected static function parseBulkPayload(Request $request): array {
+		$batch = [];
+		$cluster = '';
+		$table = '';
+		$rows = explode("\n", trim($request->payload));
+		$rowCount = sizeof($rows);
+
+		for ($i = 0; $i < $rowCount; $i++) {
+			if (empty($rows[$i])) {
+				continue;
+			}
+
+			/** @var Struct<int|string,array<string,mixed>> $struct */
+			$struct = Struct::fromJson($rows[$i]);
+
+			if (isset($struct['index']['_index'])) { // _bulk
+				/** @var string $table */
+				$table = $struct['index']['_index'];
+				[$cluster, $table] = static::parseCluster($table);
+				$batch["$cluster:$table"][] = $struct;
+				continue;
+			}
+
+			if (isset($struct['insert'])) { // bulk
+				/** @var string $table */
+				$table = $struct['insert']['table'] ?? $struct['insert']['index'];
+				[$cluster, $table] = static::parseCluster($table);
+				$batch["$cluster:$table"][] = $struct;
+				continue;
+			}
+
+			if (!$table) {
+				throw new QueryParseError('Cannot find table name');
+			}
+
+			$batch["$cluster:$table"][] = $struct;
+		}
+		/** @var Batch $batch */
+		return $batch;
+	}
+
+	/**
+	 * @param Request $request
+	 * @return Batch
+	 * @throws QueryParseError
+	 */
+	protected static function parseSinglePayload(Request $request): array {
+		$struct = Struct::fromJson($request->payload);
+		/** @var string $table */
+		$table = $struct['table'] ?? $struct['index'];
+
+		// We support 2 ways of cluster: as key or in table as prefix:
+		/** @var string $cluster */
+		$cluster = $struct['cluster'] ?? '';
+		if (!$cluster) {
+			[$cluster, $table] = static::parseCluster($table);
+		}
+
+		return ["$cluster:$table" => [$struct]];
+	}
+
+	/**
+	 * Parse the SQL request
+	 * @param Request $request
+	 * @return Batch
+	 */
+	protected static function parseSqlPayload(Request $request): array {
+		preg_match('/^insert\s+into\s+`?([^ ]+?)`?(?:\s+\(([^)]+)\))?\s+values/ius', $request->payload, $matches);
+		$table = $matches[1] ?? null;
+		$fields = [];
+		if (isset($matches[2])) {
+			$fields = array_map('trim', explode(',', $matches[2]));
+			$fields = array_map(
+				function ($field) {
+					return trim($field, '`');
+				}, $fields
+			);
+		}
+		if (!$table) {
+			throw QueryParseError::create('Failed to parse table from the query');
+		}
+		[$cluster, $table] = static::parseCluster($table);
+
+		// It's time to parse values
+		$pattern = '/values\s*\((.*)\)/ius';
+		preg_match($pattern, $request->payload, $matches);
+
+		if (!isset($matches[1])) {
+			throw QueryParseError::create('Failed to parse values from the query');
+		}
+
+		$values = $matches[1];
+		$pattern = "/'(?:[^'\\\\]*(?:\\\\.[^'\\\\]*)*)'|\\d+(?:\\.\\d+)?|NULL|\\{(?:[^{}]|\\{[^{}]*\\})*\\}/";
+		preg_match_all($pattern, $values, $matches);
+		$values = array_map(trim(...), $matches[0]);
+		$batch = [];
+
+		$fieldCount = sizeof($fields);
+		$valueCount = sizeof($values);
+		for ($i = 0; $i < $valueCount; $i += $fieldCount) {
+			$slice = array_slice($values, $i, $fieldCount);
+			$doc = array_combine($fields, $slice);
+			if (isset($doc['id'])) {
+				$doc['id'] = (int)$doc['id'];
+			}
+			$struct = Struct::fromData(['insert' => $doc]);
+			$batch[] = $struct;
+		}
+		/** @var Batch */
+		return ["$cluster:$table" => $batch];
+	}
+
+	/**
+	 * @param string $table
+	 * @return array{0:string,1:string}
+	 */
+	public static function parseCluster(string $table): array {
+		$cluster = '';
+		$pos = strpos($table, ':');
+		if ($pos !== false) {
+			$cluster = substr($table, 0, $pos);
+			$table = substr($table, $pos + 1);
+		}
+		return [$cluster, $table];
+	}
+}
