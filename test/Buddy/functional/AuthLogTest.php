f6a3c96ae3217f1bb96840e285590c26aea0abcf
djklim87 <klim@manticoresearch.com>
2026-01-14 14:10:07 +0100

    Debug

--- test/Buddy/functional/AuthLogTest.php
+++ test/Buddy/functional/AuthLogTest.php
@@ -73,7 +73,7 @@ final class AuthLogTest extends TestCase {
 			"CLUSTER or FUNCTION or PLUGIN or TABLE near 'USER";
 		$buddyResponse = static::runHttpBuddyRequest($query, ['message' => $originalError]);
 		$this->assertArrayHasKey('log', $buddyResponse);
-
+    echo "Debug error: --- ".json_encode($buddyResponse['log'], JSON_PRETTY_PRINT);
 		// Then, try to observe it in Manticore logs (requires daemon-side support).
 		// Make a failed auth attempt to force auth log file creation.
 		$port = static::getListenHttpPort();
